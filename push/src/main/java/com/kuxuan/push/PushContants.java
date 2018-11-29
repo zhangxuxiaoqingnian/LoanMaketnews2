7447c645dd580bf12b10e3db6ef9eb37f3680cfd
Xieshengqi <xieshengqi@e7124.com>
2018-11-29 15:31:04 +0800

    无修改

--- push/src/main/java/com/kuxuan/push/PushContants.java
+++ push/src/main/java/com/kuxuan/push/PushContants.java
@@ -7,6 +7,8 @@ package com.kuxuan.push;
 public class PushContants {
     public static final String XIAOMI_APP_ID = "2882303761517889517";
     public static final String XIAOMI_APP_KEY = "5621788999517";
+    public static final String OPPO_APP_ID = "3715739";
+    public static final String OPPO_APP_KEY = "ead8af33b9084458b09e112d4bf348b0";
 
 
 }
