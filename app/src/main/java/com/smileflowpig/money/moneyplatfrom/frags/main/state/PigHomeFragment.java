83137b706dcb717212264d588959213554f5a67a
Xieshengqi <xieshengqi@e7124.com>
2018-12-24 16:27:42 +0800

    无修改

--- app/src/main/java/com/smileflowpig/money/moneyplatfrom/frags/main/state/PigHomeFragment.java
+++ app/src/main/java/com/smileflowpig/money/moneyplatfrom/frags/main/state/PigHomeFragment.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
